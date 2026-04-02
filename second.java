d32149e77d928331a5a630bd42b8381b497778ca
Poleboina Bhavani Kumar <141429544+BhavaniKumarPoleboina@users.noreply.github.com>
2026-04-02 17:14:41 +0530

    Update second.java

--- second.java
+++ second.java
@@ -1,5 +1,6 @@
 class second{
     public static void main(String[] args){
         System.out.println("This is the second java file");
+        System.out.println("This is an java updated file");
     }
 }
